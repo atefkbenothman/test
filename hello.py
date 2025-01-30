6c2f431a5dec451a9b6353482240d3f9f3ff30da
Atef Kai Benothman <92714853+atefkbenothman@users.noreply.github.com>
2025-01-29 23:27:09 -0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+"""A simple module that contains a hello world function."""
+
+def hello_world():
+    """Prints a hello message to the console."""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    hello_world()
